9747acc7fe927bca8dd149dc9ad83fb341a52c30
ikeda042 <ikeda042main@gmail.com>
2024-05-30 20:17:09 +0900

    init

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+
+
+class GCdata:
+    def __init__(self, date_acquired: str, samle_name: str) -> None:
+        self.date_acquired: str = date_acquired
+        self.sample_name: str = samle_name
+        try:
+            self.pressure = float(
+                self.sample_name.split("_")[-1]
+                .replace("bar", "")
+                .replace("\n", "")
+                .replace(" ", "")
+                .replace("s", "")
+            )
+        except:
+            self.pressure: float = 0.0
+        self.data: list[str] = None
+
+    def __repr__(self) -> str:
+        return f"{self.date_acquired},{self.sample_name},{self.pressure})"
+
+    def set_data(self, data: str) -> None:
+        self.data = data
+
+
+try:
+    os.mkdir("data")
+except:
+    pass
+with open("ASCIIData_HMC53L001.txt", "r") as fp:
+    data = [i.replace("\n", "") for i in fp.readlines() if i.strip() != ""]
+
+num_data: int = 0
+header_indices: list[int] = []
+for n, i in enumerate(data):
+    if "[Header]" in i:
+        num_data += 1
+        header_indices.append(n)
+
+
+for header_index in range(len(header_indices) - 1):
+    with open(f"data/data_{header_index}.txt", "w") as fp:
+        for i in data[header_indices[header_index] : header_indices[header_index + 1]]:
+            fp.write(i + "\n")
+
+
+with open(f"data/data_{num_data - 1}.txt", "w") as fp:
+    for i in data[header_indices[-1] :]:
+        fp.write(i + "\n")
+
+
+gc_data_: list[GCdata] = []
+for data_file in os.listdir("data"):
+    with open(f"data/{data_file}", "r") as fp:
+        data = [i.replace("\n", "") for i in fp.readlines() if i.strip() != ""]
+    gc_data = GCdata(data[14], data[17])
+    gc_data.set_data(data[19:])
+    gc_data_.append(gc_data)
+    print(gc_data.data)
