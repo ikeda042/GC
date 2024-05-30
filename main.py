3a59c5930fd9059217b510a9d8f6e176e3a2780d
ikeda042 <ikeda042main@gmail.com>
2024-05-30 21:09:41 +0900

    ref

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from natsort import natsorted
 import shutil
-from __future__ import annotations
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,6 +14,18 @@ def init():
         os.mkdir("data")
     except:
         pass
+
+
+class PPData:
+    def __init__(
+        self,
+        pp_header: list[str],
+        pp_data: list[str],
+        pp: list[float],
+    ) -> None:
+        self.pp_header: list[str] = pp_header
+        self.conc: list[float] = pp_data
+        self.pp: list[float] = pp
 
 
 class GCdata:
@@ -33,9 +44,7 @@ class GCdata:
             self.pressure: float = -np.inf
         self.header: list[str] = None
         self.data: list[str] = None
-
-    def __repr__(self) -> str:
-        return f"{self.date_acquired},{self.sample_name},{self.pressure})"
+        self.ppdata: PPData = None
 
     def set_data(self, data: list[str]) -> None:
         self.data = data
@@ -43,78 +52,63 @@ class GCdata:
     def set_header(self, header: list[str]) -> None:
         self.header = header
 
+    def set_ppdata(self, ppdata: PPData) -> None:
+        self.ppdata = ppdata
 
-with open("ASCIIData_HMC53L002.txt", "r", encoding="ISO-8859-1") as fp:
-    data = [i.replace("\n", "") for i in fp.readlines() if i.strip() != ""]
-
-num_data: int = 0
-header_indices: list[int] = []
-for n, i in enumerate(data):
-    if "[Header]" in i:
-        num_data += 1
-        header_indices.append(n)
+    def __repr__(self) -> str:
+        return f"{self.date_acquired},{self.sample_name},{self.pressure})"
 
 
-for header_index in range(len(header_indices) - 1):
-    with open(f"data/data_{header_index}.txt", "w") as fp:
-        for i in data[header_indices[header_index] : header_indices[header_index + 1]]:
-            fp.write(i + "\n")
+filename = "ASCIIData_HMC53L002.txt"
 
 
-with open(f"data/data_{num_data - 1}.txt", "w") as fp:
-    for i in data[header_indices[-1] :]:
-        fp.write(i + "\n")
-
-
-gc_data_: list[GCdata] = []
-for data_file in natsorted(os.listdir("data")):
-    with open(f"data/{data_file}", "r") as fp:
+def load_data(filename: str) -> list[str]:
+    init()
+    with open(filename, "r", encoding="ISO-8859-1") as fp:
         data = [i.replace("\n", "") for i in fp.readlines() if i.strip() != ""]
-    gc_data = GCdata(data[14], data[17])
-    gc_data.set_data(data[74:])
-    gc_data.set_header(data[73].split(","))
-    gc_data_.append(gc_data)
+
+    header_indices = [n for n, i in enumerate(data) if "[Header]" in i]
+    num_data = sum(1 for _ in header_indices)
+
+    for h in range(len(header_indices)):
+        start = header_indices[h]
+        end = header_indices[h + 1] if h + 1 < len(header_indices) else None
+        with open(f"data/data_{h}.txt", "w") as fp:
+            for i in data[start:end]:
+                fp.write(i + "\n")
+
+    gc_data_: list[GCdata] = []
+    for data_file in natsorted(os.listdir("data")):
+        with open(f"data/{data_file}", "r") as fp:
+            data = [i.replace("\n", "") for i in fp.readlines() if i.strip() != ""]
+        gc_data = GCdata(data[14], data[17])
+        gc_data.set_data(data[74:])
+        gc_data.set_header(data[73].split(","))
+        gc_data_.append(gc_data)
 
 
-print(gc_data_[0].header)
-print(gc_data_[0].data)
+# pp_data: list[PPData] = []
+# for n, i in enumerate(gc_data_):
+#     print(i.sample_name)
+#     print(i.header)
+#     print(i.data)
+#     pp_header: list[str] = []
+#     conc: list[float] = []
+#     for k in i.data:
+#         conc.append(float(k.split(",")[7]))
+#         pp_header.append(k.split(",")[10])
+#     pp: list[float] = [i.pressure * j / 100 for i, j in zip(gc_data_, conc)]
+#     print(pp_header)
+#     print(pp)
+#     print(conc)
+#     pp_data.append(PPData(i.sample_name, pp_header, conc, pp))
 
 
-class PPData:
-    def __init__(
-        self,
-        sample_name: str,
-        pp_header: list[str],
-        pp_data: list[str],
-        pp: list[float],
-    ) -> None:
-        self.sample_name: str = sample_name
-        self.pp_header: list[str] = pp_header
-        self.conc: list[float] = pp_data
-        self.pp: list[float] = pp
+# pp_data_sets = [pp_data[i : i + 3] for i in range(0, len(pp_data), 3)]
 
-
-pp_data: list[PPData] = []
-for n, i in enumerate(gc_data_):
-    print(i.sample_name)
-    print(i.header)
-    print(i.data)
-    pp_header: list[str] = []
-    conc: list[float] = []
-    for k in i.data:
-        conc.append(float(k.split(",")[7]))
-        pp_header.append(k.split(",")[10])
-    pp: list[float] = [i.pressure * j / 100 for i, j in zip(gc_data_, conc)]
-    print(pp_header)
-    print(pp)
-    print(conc)
-    pp_data.append(PPData(i.sample_name, pp_header, conc, pp))
-
-
-pp_data_sets = [pp_data[i : i + 3] for i in range(0, len(pp_data), 3)]
-
-for i in pp_data_sets:
-    print(i[0].sample_name)
-    print(i[1].sample_name)
-    print(i[2].sample_name)
-    print("+++++++++++++++++++++++")
+# for i in pp_data_sets:
+#     print(i[0].sample_name)
+#     print(i[1].sample_name)
+#     print(i[2].sample_name)
+#     print("+++++++++++++++++++++++")
+load_data(filename)
